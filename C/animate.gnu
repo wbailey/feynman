ddfe79d9c15e77a33c64aa926fdbc83370524751
Wes Bailey <baywes@gmail.com>
2014-07-24 23:34:11 -0700

    cleaning up and moving to functions

--- C/animate.gnu
+++ C/animate.gnu
@@ -2,19 +2,19 @@ set xrange [-1:10]
 set yrange [0:11]
 num_lines="`wc -l data.csv`"
 
-set terminal png
+#set terminal png
 
-set output 'png/animate000.png'
+#set output 'png/animate000.png'
 
 plot 'data.csv' every 1::0::0 using 2:3 w cir title '' \
    , 'data.csv' every 1::0::0 using 4:5 w cir title ''
 
 do for [i=1:num_lines] {
-  set output sprintf('png/animate%03.0f.png',i)
+#  set output sprintf('png/animate%03.0f.png',i)
   plot 'data.csv' every 1::i-1::i using 2:3 w cir lc 000000 title '' \
      , 'data.csv' every 1::i-1::i using 4:5 w cir lc 000000 title ''
   pause 0.05000
 }
 
-set terminal x11
-set output
+#set terminal x11
+#set output
--- C/runner.c
+++ C/runner.c
@@ -4,31 +4,42 @@
 
 static const int collection_size = 2;
 
+void accel(Ball *ball) {
+  const double g = 9.80;
+
+  ball->ay = -g;
+}
+
+void euler(Ball *ball, double dt) {
+  accel(ball);
+  ball->vx += ball->ax * dt;
+  ball->x  += ball->vx * dt;
+  ball->vy += ball->ay * dt;
+  ball->y  += ball->vy * dt;
+}
+
+void initialize(BallCollection *ball) {
+  // Initial Conditions
+  ball[0]->y = 10.0;
+  ball[1]->y = 10.0;
+  ball[1]->vx = 5.0;
+}
+
 int main() {
   BallCollection *ball;
-  const double g = 9.8, dt = 0.01;
+  const double dt = 0.01;
   double t = 0.0;
   int j;
 
   ball = new_BallCollection(collection_size);
 
-  // Initial Conditions
-  ball[0]->y = 10.0;
-  ball[1]->y = 10.0;
-  ball[1]->vx = 5.0;
-
-  ball[0]->ay = -g;
-  ball[1]->ay = -g;
+  initialize(ball);
 
   while (ball[0]->y > 0.0 && ball[1]->y > 0.0) {
     printf("%12.6f ", t);
 
     for (j = 0; j < 2; j++) {
-      ball[j]->vx += ball[j]->ax * dt;
-      ball[j]->x += ball[j]->vx * dt;
-      ball[j]->vy += ball[j]->ay * dt;
-      ball[j]->y += ball[j]->vy * dt;
-
+      euler(ball[j], dt);
       printf("%12.6f %12.6f ", ball[j]->x, ball[j]->y);
     }
 
