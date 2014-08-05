0e5140dd56886ac01fded85e01e9b5abd7cd36ce
Wes Bailey <baywes@gmail.com>
2014-08-04 17:35:06 -0700

    correct algorithm implementation

--- C/molecular_dynamics/include/simulation.h
+++ C/molecular_dynamics/include/simulation.h
@@ -8,12 +8,12 @@ double LJ_Epsilon = 0.997; // kJ/mol
 double LJ_Sigma = 3.40;    // Angstroms
 
 double t = 0;
-double dt = 0.001;
+double dt = 0.01;
 
 double MD_Collection_Size = 3;
 double MD_Box_Length = 18;
 
-int MD_Iterations  = 20000;
+int MD_Iterations  = 2000;
 int MD_ReportCount = 1000;
 
 #endif
--- C/molecular_dynamics/runner.c
+++ C/molecular_dynamics/runner.c
@@ -34,7 +34,7 @@ void print_Report(Report *report) {
 }
 
 int main(void) {
-  double pe, ke;
+  double r, pe, ke;
   ParticleCollection *particle = new_ParticleCollection(MD_Collection_Size);
   MD_Separation *sep;
   MD_Accel *accel;
@@ -51,30 +51,40 @@ int main(void) {
 
     MD_reset_Collection_Accel(particle, MD_Collection_Size);
 
+    // Calculate the cumalative force on each particle
     for (int m = 0; m < MD_Collection_Size - 1; m++) {
       for (int n = m + 1; n < MD_Collection_Size; n++) {
-        sep   = MD_new_Separation(particle[m], particle[n], MD_Box_Length);
-
-        MD_toString_Separation(sep);
+        sep = MD_new_Separation(particle[m], particle[n], MD_Box_Length);
+        r = MD_calculate_R(sep);
 
         accel = MD_new_Accel(sep);
 
-        pe += LJ_Potential_Energy(MD_calculate_R(sep));
+        pe += LJ_Potential_Energy(r);
 
-        MD_iterate_Euler(particle[m], accel, dt);
-        MD_thirdlaw_Accel(accel);
-        MD_iterate_Euler(particle[n], accel, dt);
+        particle[m]->ax += accel->ax;
+        particle[m]->ay += accel->ay;
+        particle[m]->az += accel->az;
 
-        MD_apply_Periodic(particle[m], MD_Box_Length);
-        MD_apply_Periodic(particle[n], MD_Box_Length);
+        particle[n]->ax -= accel->ax;
+        particle[n]->ay -= accel->ay;
+        particle[n]->az -= accel->az;
 
         MD_destroy_Separation(sep);
         MD_destroy_Accel(accel);
       }
     }
 
-    // Euler iteration should be happening here after all of the accelerations
-    // have been summed per particle
+    for (int m = 0; m < MD_Collection_Size; m++) {
+      particle[m]->vx += particle[m]->ax * dt;
+      particle[m]->vy += particle[m]->ay * dt;
+      particle[m]->vz += particle[m]->az * dt;
+
+      particle[m]->x  += particle[m]->vx * dt;
+      particle[m]->y  += particle[m]->vy * dt;
+      particle[m]->z  += particle[m]->vz * dt;
+
+      MD_apply_Periodic(particle[m], MD_Box_Length);
+    }
 
     t += dt;
     ke = MD_calc_Kinetic_Energy(particle, MD_Collection_Size);
