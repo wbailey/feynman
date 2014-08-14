87c49dd97de7eb246f1e9e308c9da8ba06be751c
Wes Bailey <baywes@gmail.com>
2014-08-14 08:43:15 -0700

    using histograms

--- C/molecular_dynamics/maxwell.py
+++ C/molecular_dynamics/maxwell.py
@@ -1,37 +1,33 @@
 #!/usr/local/bin/python3
 import sys
-from pylab import plot,show
+import matplotlib.pyplot as pyplot
+import numpy as np
 
-velocities = sys.stdin.readlines()
+velocities = np.loadtxt(sys.stdin)
 
-# the last line in the file contains
-# vmax     5.6798
-#
-# so this is a dirty trick
-vmax = float(velocities.pop().split()[1])
+# bin_size = vmax/bin_count
+# bins = {}
 
-bin_count = 200
-bin_size = vmax/bin_count
-bins = {}
+# for v in velocities:
+#   # v = float(lines.split()[1])
 
-for lines in velocities:
-  v = float(lines.split()[1])
+#   index = int(v/bin_size)
 
-  index = int(v/bin_size)
+#   if index in bins:
+#     bins[index] += 1
+#   else:
+#     bins[index] = 1
 
-  if index in bins:
-    bins[index] += 1
-  else:
-    bins[index] = 1
+pyplot.hist(velocities, bins = 20)
+pyplot.show()
 
-x = []
-y = []
+# sys.exit(0)
+# x = []
+# y = []
 
-print(bins)
+# for key, value in bins.items():
+#   x.append(key * bin_size)
+  # y.append(bin_count * value/len(velocities))
 
-for key, value in bins.items():
-  x.append(key * bin_size)
-  y.append(bin_count * value/len(velocities))
-
-plot(x,y)
-show()
+# plot(x,y)
+# show()
