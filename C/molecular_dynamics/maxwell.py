1aa5b6d9e859e954a253990c6bc972ae3ef9c32b
Wes Bailey <wes@sproutling.com>
2016-02-19 13:25:55 -0800

    binning

--- C/molecular_dynamics/maxwell.py
+++ C/molecular_dynamics/maxwell.py
@@ -5,29 +5,29 @@ import numpy as np
 
 velocities = np.loadtxt(sys.stdin)
 
-# bin_size = vmax/bin_count
-# bins = {}
+bin_size = vmax/bin_count
+bins = {}
 
-# for v in velocities:
-#   # v = float(lines.split()[1])
+for v in velocities:
+  v = float(lines.split()[1])
 
-#   index = int(v/bin_size)
+  index = int(v/bin_size)
 
-#   if index in bins:
-#     bins[index] += 1
-#   else:
-#     bins[index] = 1
+  if index in bins:
+    bins[index] += 1
+  else:
+    bins[index] = 1
 
-pyplot.hist(velocities, bins = 20)
-pyplot.show()
+# pyplot.hist(velocities, bins = 20)
+# pyplot.show()
 
-# sys.exit(0)
-# x = []
-# y = []
+sys.exit(0)
+x = []
+y = []
 
-# for key, value in bins.items():
-#   x.append(key * bin_size)
-  # y.append(bin_count * value/len(velocities))
+for key, value in bins.items():
+  x.append(key * bin_size)
+  y.append(bin_count * value/len(velocities))
 
-# plot(x,y)
-# show()
+plot(x,y)
+show()
